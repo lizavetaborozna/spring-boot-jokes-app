cb4c20d5f548f0f27142c0ae392e524afe390f11
PK <pavel.khmialinku@standardbank.co.za>
2022-11-08 22:45:30 +0300

    Initial commit

---
+++ src/test/java/com/joke/app/jokeapp/JokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joke.app.jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
